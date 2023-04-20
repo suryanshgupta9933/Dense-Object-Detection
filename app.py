3784e73dfe531a8f8ec619c8f44a277009b82be0
Suryansh Gupta <suryanshgupta9933@gmail.com>
2023-04-20 18:32:18 +0530

    updated streamlit app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from yolov5.utils.general import non_max_suppression
 from yolov5.utils.torch_utils import select_device
 
 # Define the path to your saved YOLOv5 model
-model_path = r"weights\best.pt"
+model_path = "/weights/best.pt"
 
 # Load the model
 device = select_device('0' if torch.cuda.is_available() else 'cpu')
